def2b27354e6341841dcffbc89369077c053ed6a
JuanDavid31 <juandavid0306@hotmail.com>
2020-02-08 18:50:00 +0100

    (Nuevo) Dockerfile funcionando

---
+++ compile-and-build-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#ng build --prod
+docker image build -t inv-frontend:1.0 .
+docker container rm inv-frontend-container
+docker container create -p 8090:80 --name inv-frontend-container inv-frontend:1.0
+docker container start -a inv-frontend-container
