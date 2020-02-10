1d91f0b831ab8390f6691401e0efc15880023317
Ubuntu <ubuntu@ip-172-31-31-123.us-east-2.compute.internal>
2020-02-10 15:25:08 +0000

    (Bug) Puliendo el .sh

--- compile-and-build-docker.sh
+++ compile-and-build-docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ng build --prod=true --aot=true
 docker image build -t inv-frontend:1.0 .
-docker container rm inv-frontend-container
+docker container stop inv-frontend-container || true # || true hace que cuando el primer comando de un error, la ejecición de los demás comando continue
+docker container rm inv-frontend-container || true # Sin problemas.
 docker container create -p 8090:80 --name inv-frontend-container inv-frontend:1.0
 docker container start -a inv-frontend-container
