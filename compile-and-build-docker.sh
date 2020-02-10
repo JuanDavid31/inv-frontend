3eeece384074651f1b88b5e21a85c11b4cb52db3
Ubuntu <ubuntu@ip-172-31-31-123.us-east-2.compute.internal>
2020-02-10 15:36:51 +0000

    (Bug) Corrigiendo puerto expuerto el Dockerfile

--- compile-and-build-docker.sh
+++ compile-and-build-docker.sh
@@ -3,5 +3,5 @@ ng build --prod=true --aot=true
 docker image build -t inv-frontend:1.0 .
 docker container stop inv-frontend-container || true # || true hace que cuando el primer comando de un error, la ejecición de los demás comando continue
 docker container rm inv-frontend-container || true # Sin problemas.
-docker container create -p 8090:80 --name inv-frontend-container inv-frontend:1.0
+docker container create -p 80:80 --name inv-frontend-container inv-frontend:1.0
 docker container start -a inv-frontend-container
