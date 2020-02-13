adea8d443a4dd201f28950137a2a8e8b12aaa73f
Ubuntu <ubuntu@ip-172-26-0-167.us-east-2.compute.internal>
2020-02-13 11:23:58 +0000

    (Bug) Eliminando comentario que impedia la build del Dockerfile a imagen

--- compile-and-build-docker.sh
+++ compile-and-build-docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ng build --prod=true --aot=true
-#docker image build -t inv-frontend:1.0 .
+docker image build -t inv-frontend:1.0 .
 docker container stop inv-frontend-container || true # || true hace que cuando el primer comando de un error, la ejecición de los demás comando continue
 docker container rm inv-frontend-container || true # Sin problemas.
 docker container create -p 80:80 --name inv-frontend-container inv-frontend:1.0
